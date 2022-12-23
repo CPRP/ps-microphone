c41fbe79394bf7d5952f0acae80c098bbec4361c
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2022-12-22 22:41:09 -0500

    added courthouse location

--- config.lua
+++ config.lua
@@ -18,5 +18,21 @@ Config.MicrophoneZones = {
                 range = 50.0 -- range for the voice
             }
         }
+    },
+    [2] = {
+        name = "courthouse", 
+        coords = vector3(-576.37, -210.64, 38.77),
+        length = 3.4,
+        width = 3.6,
+        spawnProp = false, -- if set to true, it will let you spawn the prop at location
+        data = {
+            debugPoly = Config.Showzone,
+            heading = 308.37,
+            minZ = 35.86,
+            maxZ = 41.86,
+            data = {
+                range = 50.0 -- range for the voice
+            }
+        }
     }
 }
